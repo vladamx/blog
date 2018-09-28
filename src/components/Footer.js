8e1d9a193084df8aed9aca006254ccb182667f1f
Vladimir Milojevic <vladamx.dev@gmail.com>
2018-09-28 17:17:43 +0200

    Fix typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 function Footer({props}) {
     return (
         <div style={{textAlign:'center'}}>
-        <small>&copy; EngineringJourney, {new Date().getFullYear()}.</small>
+        <small>&copy; EngineeringJourney, {new Date().getFullYear()}.</small>
         </div>
     )
 }
